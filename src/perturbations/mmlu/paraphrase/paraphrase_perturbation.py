ee2e1256362fb9a40955a0862e35368bb19cbd43
bogdankostic <bogdankostic@web.de>
2025-06-11 15:23:32 +0200

    Add paraphrase perturbation job and implementation for MMLU dataset

---
+++ src/perturbations/mmlu/paraphrase/paraphrase_perturbation.py
@@ -0,0 +1,100 @@
+from collections import defaultdict
+from datetime import datetime
+import os
+import subprocess
+import json
+from typing import List
+
+import pandas as pd
+from datasets import load_dataset, get_dataset_config_names
+from haystack import Pipeline
+from haystack.components.builders.chat_prompt_builder import ChatPromptBuilder
+from haystack.dataclasses import ChatMessage
+from haystack.utils import Secret
+from pydantic import BaseModel
+from tqdm import tqdm
+
+from src.custom_components.generator.cached_chat_generator import CachedOpenAIChatGenerator
+
+
+class MultipleChoiceQuestion(BaseModel):
+    question: str
+    choices: List[str]
+
+
+json_schema = MultipleChoiceQuestion.model_json_schema()
+answer_mapping = ["A", "B", "C", "D"]
+
+
+# Construct perturbation pipeline
+prompt_template = [ChatMessage.from_user("""You are given a multiple-choice question. 
+Your task is to rewrite the question and its answer choices using different wording and syntactic structures, but without changing the original meaning. 
+Ensure the paraphrased version remains clear and preserves the structure of a multiple-choice question with four answer options.
+Do not add or remove any information.
+
+Question: {{question}}
+{% for choice in choices %}{{ ['A', 'B', 'C', 'D'][loop.index0] }}) {{choice}}
+{% endfor %}
+Answer:
+""")]
+
+prompt_builder = ChatPromptBuilder(
+    template=prompt_template
+)
+paraphrase_model = CachedOpenAIChatGenerator(
+    api_key=Secret.from_env_var("PLACEHOLDER"),
+    model="meta-llama/Llama-3.2-3B-Instruct",
+    cache_dir="tmp/llm-cache",
+    api_base_url=f"{os.environ['LLM_MODEL_ENDPOINT']}/v1",
+    generation_kwargs={
+        "temperature": 0,
+        "seed": 77,
+        "extra_body": {
+            "guided_json": json_schema
+        }
+    }
+)
+
+perturbation_pipeline = Pipeline()
+perturbation_pipeline.add_component(name="prompt_builder", instance=prompt_builder)
+perturbation_pipeline.add_component(name="paraphrase_model", instance=paraphrase_model)
+perturbation_pipeline.connect("prompt_builder.prompt", "paraphrase_model.messages")
+
+
+# Perturb dataset
+perturbed_data = defaultdict(list)
+dataset_names = get_dataset_config_names("tasksource/mmlu")
+for dataset_name in tqdm(dataset_names, desc="Dataset"):
+    data = load_dataset("tasksource/mmlu", name=dataset_name, split="test")
+    for sample in tqdm(data, desc="Sample"):
+        response = perturbation_pipeline.run({
+            "prompt_builder": {
+                "question": sample["question"],
+                "choices": sample["choices"]
+            }
+        })
+
+        response_dict = json.loads(response["paraphrase_model"]["replies"][0].text)
+        question = response_dict["question"]
+        choices = response_dict["choices"]
+        answer = answer_mapping[sample["answer"]]
+        perturbed_data["question"].append(question)
+        perturbed_data["choices"].append(choices)
+        perturbed_data["answer"].append(answer)
+        perturbed_data["dataset_name"].append(dataset_name)
+
+# Save perturbed data
+perturbed_data_df = pd.DataFrame(perturbed_data)
+output = {
+    "metadata": {
+        "description": f"MMLU dataset perturbed with paraphrased questions and answer choices",
+        "commit_hash": subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip(),
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    },
+    "data": perturbed_data_df.to_dict(orient="records")
+}
+
+output_path = "/experiments/data/mmlu/paraphrase/paraphrase_perturbation.json"
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+with open(output_path, "w") as f:
+    json.dump(output, f)
