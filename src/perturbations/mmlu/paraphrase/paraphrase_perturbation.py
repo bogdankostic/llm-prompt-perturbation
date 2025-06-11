dbebff42e5ce33f289c162f511f244bbc0926be2
bogdankostic <bogdankostic@web.de>
2025-06-11 16:22:36 +0200

    Add Kubernetes deployment and service configurations for Llama-3.1-8B Instruct model, and update paraphrase perturbation endpoint to reflect the new service.

--- src/perturbations/mmlu/paraphrase/paraphrase_perturbation.py
+++ src/perturbations/mmlu/paraphrase/paraphrase_perturbation.py
@@ -31,7 +31,10 @@ answer_mapping = ["A", "B", "C", "D"]
 prompt_template = [ChatMessage.from_user("""You are given a multiple-choice question. 
 Your task is to rewrite the question and its answer choices using different wording and syntactic structures, but without changing the original meaning. 
 Ensure the paraphrased version remains clear and preserves the structure of a multiple-choice question with four answer options.
-Do not add or remove any information.
+IMPORTANT: Do not add or remove any information.
+The output should be a valid JSON object with the following fields:
+- question: str  # The paraphrased question containing all relevant information
+- choices: List[str]  # The paraphrased answer choices
 
 Question: {{question}}
 {% for choice in choices %}{{ ['A', 'B', 'C', 'D'][loop.index0] }}) {{choice}}
@@ -65,10 +68,10 @@ perturbation_pipeline.connect("prompt_builder.prompt", "paraphrase_model.message
 # Perturb dataset
 perturbed_data = defaultdict(list)
 dataset_names = get_dataset_config_names("tasksource/mmlu")
-for dataset_name in tqdm(dataset_names, desc="Dataset", file=sys.stdout, dynamic_ncols=False, ncols=80):
+for dataset_name in tqdm(dataset_names, desc="Dataset"):
     print(f"Processing dataset: {dataset_name}")
     data = load_dataset("tasksource/mmlu", name=dataset_name, split="test")
-    for sample in tqdm(data, desc="Sample", file=sys.stdout, dynamic_ncols=False, ncols=80):
+    for sample in tqdm(data, desc="Sample"):
         response = perturbation_pipeline.run({
             "prompt_builder": {
                 "question": sample["question"],
@@ -79,6 +82,9 @@ for dataset_name in tqdm(dataset_names, desc="Dataset", file=sys.stdout, dynamic
         response_dict = json.loads(response["paraphrase_model"]["replies"][0].text)
         question = response_dict["question"]
         choices = response_dict["choices"]
+        for idx, choice in enumerate(choices):
+            if choice[:2] in ["A)", "B)", "C)", "D)"]:
+                choices[idx] = choice[2:].strip()
         answer = answer_mapping[sample["answer"]]
         perturbed_data["question"].append(question)
         perturbed_data["choices"].append(choices)
